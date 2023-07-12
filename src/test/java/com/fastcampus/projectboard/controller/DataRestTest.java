71d27fd9f470d615fb88d29b68736f7e90b38a67
KimSangwoo12 <rlatkddn2902@gmail.com>
2023-07-12 19:50:05 +0900

    #1 - Spring Data REST 테스트 제외 처리
    비즈니스 로직이 아닌 data rest 기능이고 통합테스트라 무거우며 db에도 영향을 준다.
    공부 목적으로도 의미는 있기 때문에 삭제하지는 않고
    제외 처리 해서 전체 테스트 중에 실행되지 않게 끔 처리

--- src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
@@ -1,23 +1,19 @@
 package com.fastcampus.projectboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcResultMatchersDsl;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled
 @DisplayName("Data REST - API 테스트")
 @Transactional
 @AutoConfigureMockMvc
