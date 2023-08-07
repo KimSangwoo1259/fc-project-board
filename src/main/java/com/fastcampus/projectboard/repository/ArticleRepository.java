06c9bdb503911e934cdf93eb9a6c64aa7906fb51
KimSangwoo12 <rlatkddn2902@gmail.com>
2023-08-08 00:43:38 +0900

    #36 - 도메인 변경점 반영: 해시태그
    도메인에서 해시태그에 대한 변경으로 인해 서비스코드들에서 변경해줘야 하는 부분들을 변경해줌

--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -5,7 +5,6 @@ import com.fastcampus.projectboard.domain.QArticle;
 import com.fastcampus.projectboard.repository.querydsl.ArticleRepositoryCustom;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,24 +17,25 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
         ArticleRepositoryCustom,
-        QuerydslPredicateExecutor<Article>, //엔티티 안에 있는 필드들의 검색기능을 추가
+        QuerydslPredicateExecutor<Article>,
         QuerydslBinderCustomizer<QArticle> {
 
     Page<Article> findByTitleContaining(String title, Pageable pageable);
     Page<Article> findByContentContaining(String content, Pageable pageable);
     Page<Article> findByUserAccount_UserIdContaining(String userId, Pageable pageable);
     Page<Article> findByUserAccount_NicknameContaining(String nickname, Pageable pageable);
-    Page<Article> findByHashtag(String hashtag, Pageable pageable);
 
     void deleteByIdAndUserAccount_UserId(Long articleId, String userid);
+
     @Override
-    default void customize(QuerydslBindings bindings, QArticle root){
-        bindings.excludeUnlistedProperties(true); // 아래 코드에서 지정한 필드를 통해서만 검색할 수 있음
-        bindings.including(root.title,root.content,root.hashtag, root.createdAt, root.createdBy);
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtags, root.createdAt, root.createdBy);
         bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
-        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtags.any().hashtagName).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.createdAt).first(DateTimeExpression::eq);
-        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
     }
+
 }
